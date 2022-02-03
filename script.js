d0f5350cb6bb8dbda43ae1e19b71c0ffb6b551de
Nikhil Nitin Ranjan <72209132+nikhilnitinranjan@users.noreply.github.com>
2022-02-03 17:05:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+dycalendar.draw ({
+    target: '#dycalendar',
+    type: 'month',
+    dayformat: 'full',
+    monthformat: 'full',
+    highlighttargetdate: true,
+    prevnextbutton: 'show',
+
+})
